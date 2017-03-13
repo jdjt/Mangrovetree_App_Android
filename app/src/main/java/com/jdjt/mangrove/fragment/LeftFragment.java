71043483b8ec9718b0ffc06e8c27f5c214dc0229
wangmingyu <wangmingyu@jdjt.net>
2017-03-13 17:13:42 +0800

    调整左侧菜单栏 和地图的整合

---
+++ app/src/main/java/com/jdjt/mangrove/fragment/LeftFragment.java
@@ -0,0 +1,33 @@
+package com.jdjt.mangrove.fragment;
+
+
+import android.support.v4.app.Fragment;
+
+import com.jdjt.mangrove.R;
+import com.jdjt.mangrovetreelibray.ioc.annotation.InLayer;
+
+/**
+ * @author wmy
+ * @Description:
+ * @FileName:LeftFragment
+ * @Package com.jdjt.mangrove.fragment
+ * @Date 2017/3/13 15:43
+ */
+@InLayer(R.layout.header_nav)
+public class LeftFragment extends Fragment {
+//    protected LayoutInflater inflater;
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        this.inflater = inflater;
+//        View rootView = inflater.inflate(R.layout.header_nav, container, false);
+//
+//        return rootView;
+//    }
+//
+//    @Nullable
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        return super.onCreateView(inflater, container, savedInstanceState);
+//    }
+}
+
