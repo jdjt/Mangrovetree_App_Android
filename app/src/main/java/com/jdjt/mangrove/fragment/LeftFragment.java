21a60c57a7663f7dbb63e0197ecf9946d5cf6b3b
huyanan <1103995626@qq.com>
2017-03-13 18:49:16 +0800

    修改constraint-layout版本

--- app/src/main/java/com/jdjt/mangrove/fragment/LeftFragment.java
+++ app/src/main/java/com/jdjt/mangrove/fragment/LeftFragment.java
@@ -1,10 +1,20 @@
 package com.jdjt.mangrove.fragment;
 
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.jdjt.mangrove.R;
+import com.jdjt.mangrove.login.PesonalInfoActivity;
+import com.jdjt.mangrove.login.SettingActivity;
+import com.jdjt.mangrovetreelibray.ioc.annotation.InBinder;
 import com.jdjt.mangrovetreelibray.ioc.annotation.InLayer;
+import com.jdjt.mangrovetreelibray.ioc.annotation.InView;
+import com.jdjt.mangrovetreelibray.ioc.annotation.Init;
+import com.jdjt.mangrovetreelibray.ioc.listener.OnClick;
 
 /**
  * @author wmy
@@ -15,6 +25,36 @@ import com.jdjt.mangrovetreelibray.ioc.annotation.InLayer;
  */
 @InLayer(R.layout.header_nav)
 public class LeftFragment extends Fragment {
+
+    @InView(binder = @InBinder(listener = OnClick.class, method = "click"))
+    LinearLayout account_islogin; //用户中心
+    @InView(R.id.account_setting_layout)
+    LinearLayout account_setting_layout;//设置
+
+
+
+    @Init
+    public void init(){
+        account_islogin.setOnClickListener((View.OnClickListener) getActivity());
+        account_setting_layout.setOnClickListener((View.OnClickListener) getActivity());
+
+    }
+    public void onClick(View view){
+        switch (view.getId()){
+            //跳转到用户中心
+            case R.id.account_islogin:
+                Toast.makeText(getContext(),"好的",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getActivity(), PesonalInfoActivity.class));
+                break;
+            //跳转到设置
+            case R.id.account_setting_layout:
+                startActivity(new Intent(getActivity(), SettingActivity.class));
+                break;
+            default:
+                break;
+        }
+    }
+
 //    protected LayoutInflater inflater;
 //    @Override
 //    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
